31d8251b9ebf9aefe973017898bac9f33ebb0d2b
legendary <legendary.phoenix.icarus@gmail.com>
2022-01-09 05:33:07 +0300

    update: withdrawMATIC function

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.11;
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 interface ICAPL {
     function mint(address _to, uint256 _amount) external;
@@ -11,6 +12,7 @@ interface ICAPL {
 
 contract Vault is Pausable, AccessControl {
     bytes32 public constant minter = keccak256("MINTER");
+    using SafeERC20 for IERC20;
 
     // owner of vault contract
     address private _owner;
@@ -18,6 +20,9 @@ contract Vault is Pausable, AccessControl {
     // access control roles definition
     // reward token
     address public capl;
+
+    // native token address based on the deployed network - in this case Polygon MATIC token
+    address public matic;
 
     // the Treasury funded via platform fees upon deposit & withdraw
     address public treasury;
@@ -42,10 +47,13 @@ contract Vault is Pausable, AccessControl {
     event WithdrawToken(address token, uint amount, address destination);
 
     // TBD: Assume creation with one pool required (?)
-    constructor (address _capl, address _treasury) {
+    constructor (address _capl, address _matic, address _treasury) {
         capl = _capl;
+        matic = _matic;
+
         _owner = msg.sender;
         treasury = _treasury;
+
         // Grant the contract deployer the default admin role: it will be able
         // to grant and revoke any roles
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
@@ -71,7 +79,7 @@ contract Vault is Pausable, AccessControl {
 
         // platform fee
         _transferDepositFee(_user, _token, _amount);
-        IERC20(_token).transferFrom(_user, address(this), _amount);
+        IERC20(_token).safeTransferFrom(_user, address(this), _amount);
         
         // update the pool info
         pool.totalPooled += _amount;
@@ -91,7 +99,7 @@ contract Vault is Pausable, AccessControl {
         require(pool.totalPooled >= _amount, "Amount: exceed the pooled amount");
 
         // withdraw the token to user wallet
-        IERC20(_token).transferFrom(address(this), _user, _amount);
+        IERC20(_token).safeTransferFrom(address(this), _user, _amount);
         
         // update the pool info
         pool.totalPooled -= _amount;
@@ -136,7 +144,7 @@ contract Vault is Pausable, AccessControl {
         require(_amount > 0 && pool.totalPooled >= _amount);
         
         // withdraw the token to user wallet
-        IERC20(_token).transferFrom(address(this), _destination, _amount);
+        IERC20(_token).safeTransferFrom(address(this), _destination, _amount);
 
         // update the pooled amount
         pool.totalPooled -= _amount;
@@ -145,13 +153,27 @@ contract Vault is Pausable, AccessControl {
     }
 
     function withdrawMATIC(address _destination) external onlyOwner {
+        Pool storage pool = Pools[matic];
+        uint256 amount = pool.totalPooled;
+        
+        require(amount > 0);
+        
+        // withdraw the token to user wallet
+        IERC20(matic).safeTransferFrom(address(this), _destination, amount);
+
+        // update the pooled amount
+        pool.totalPooled -= amount;
+
+        emit WithdrawToken(matic, amount, _destination);
     }
 
     function withdrawAllVault(address _user, address _token) external onlyOwner {
         Pool storage pool = Pools[_token];
 
+        require(pool.totalPooled > 0, "Balance: 0");
+
         // withdraw the token to user wallet
-        IERC20(_token).transferFrom(address(this), _user, pool.totalPooled);
+        IERC20(_token).safeTransferFrom(address(this), _user, pool.totalPooled);
 
         pool.totalPooled = 0;
         pool.totalUsers -= 1;
@@ -163,6 +185,6 @@ contract Vault is Pausable, AccessControl {
     function _transferDepositFee(address _user, address _token, uint256 _amount) private {
         uint currentDepositFee = (_amount * depositFee) / ONE_HUNDRED;
         _amount -= currentDepositFee;
-        IERC20(_token).transferFrom(_user, treasury, currentDepositFee);
+        IERC20(_token).safeTransferFrom(_user, treasury, currentDepositFee);
     }
 }
