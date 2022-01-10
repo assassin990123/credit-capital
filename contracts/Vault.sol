ca004d290a25b4d4c1d8034581cfe72804bd3702
Elo <elo@Elos-MacBook-Pro.local>
2022-01-09 21:02:36 -0800

    vault update

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -4,20 +4,26 @@ pragma solidity 0.8.11;
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract Vault is Pausable, AccessControl {
-    // stake tracking
-    mapping (address => Stake[]) Stakes;
+    using SafeERC20 for IERC20;
+    
+    uint256 timelock = 137092276;   // 4 years, 4 months, 4 days ...
 
     struct Stake {
-        uint256 key;             // stake identifier
         uint256 amount;          // quantity staked
         uint256 startBlock;      // stake creation timestamp
         uint256 timeLockEnd;     // The point at which the (4 yr, 4 mo, 4 day) timelock ends for a stake, and thus the funds can be withdrawn.
         bool active;             // true = stake in vault, false = user withdrawn stake
     }
     // user position tracking
-    mapping(address => UserPosition[]) Users;
+    mapping(address => UserPosition[]) UserPositions;
+
+
+    mapping(address => mapping(uint256 => Stake[])) Stakes;
+    // users stake identifiers
 
     struct UserPosition {
         address token;           // MRC20 associated with pool
@@ -25,7 +31,7 @@ contract Vault is Pausable, AccessControl {
         uint256 pendingRewards;  // total rewards pending for user 
         uint256 rewardDebt;      // house fee (?)
         uint256 claimedRewards;  // total rewards claimed by user for given pool
-        Stake[] stakes;          // list of user stakes in pool subject to timelock
+        uint256[] sKey;          // list of user stakes in pool subject to timelock
         bool staticLock;         // guarantees a users stake is locked, even after timelock expiration
     }
 
@@ -50,6 +56,44 @@ contract Vault is Pausable, AccessControl {
     function depositVault(address _token, uint256 _amount) external {}
 
     function withdrawVault(address _token, uint256 _amount) external {}
+
+
+    /**
+        @dev - here we can assume that there are no timelocks, since the vault has no knowledge of the pool.
+     */
+    function depositStakeNew(address _token, uint256 _amount, uint256 _rewardsPerDay) external {
+        require(!checkIfPoolExists(_token), "This pool already exists.");
+        // create user & stake data
+        Stake memory newStake = Stake({
+            amount: _amount,                   // first stake
+            startBlock: block.timestamp,
+            timeLockEnd: block.timestamp + timelock,
+            active: true
+        });
+
+        uint256[] memory userStakeKeys;
+
+        UserPosition memory newUser = UserPosition ({
+            token: _token,
+            totalAmount: _amount,
+            pendingRewards: 0,
+            rewardDebt: 0,
+            claimedRewards: 0,
+            sKey: userStakeKeys,
+            staticLock: false
+        });
+        // persist user
+        UserPositions[msg.sender].push(newUser);
+        // add new stake key
+        UserPositions[msg.sender][0].sKey.push(0);
+        // register users Stake
+        Stakes[msg.sender][0].push(newStake);
+
+        // transfer funds to the vault
+        IERC20(_token).safeTransfer(address(this), _amount);
+
+        addPool(_token, _amount, _rewardsPerDay);
+    }
    
     /*
         Read functions
@@ -64,22 +108,20 @@ contract Vault is Pausable, AccessControl {
         return Pools[_token].totalUsers > 0;
     }
 
-
     /*  This function will check if a new stake needs to be created based on lockingThreshold.
         See readme for details.
     */
     function checkTimelockThreshold() internal returns (bool) {}
-   
     /*
         Admin functions
         TODO: Add RBAC for all
     */
-    function addPool(address _token, uint256 _amount) external {
+    function addPool(address _token, uint256 _amount, uint256 _rewardsPerDay) public {
         require(!checkIfPoolExists(_token), "This pool already exists");
         Pools[_token] = Pool({
             totalPooled: _amount,
             totalUsers: 1,
-            active: true
+            rewardsPerDay: _rewardsPerDay
         });
     }
 
@@ -88,8 +130,4 @@ contract Vault is Pausable, AccessControl {
     function withdrawToken(address _token, uint256 _amount, address _destination) external {}
 
     function withdrawMATIC(address _destination) external {}
-
-    function withdrawAllVault(address _token) external {}
-
-
 }
