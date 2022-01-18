21b8622bcb89416a3e5b3df16e8057295ace916c
legendary <legendary.phoenix.icarus@gmail.com>
2022-01-19 02:47:20 +0300

    Refactor: CreditCapitalPlatformToken contract
    * pass cap_ to the constructor as using ERC20Capped extension
    * override _mint function of ERC20, ERC20Capped

--- contracts/tokens/CreditCapitalPlatformToken.sol
+++ contracts/tokens/CreditCapitalPlatformToken.sol
@@ -3,41 +3,29 @@ pragma solidity ^0.8.2;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract CreditCapitalPlatformToken is ERC20, ERC20Burnable, AccessControl {
+contract CreditCapitalPlatformToken is ERC20, ERC20Burnable, ERC20Capped, AccessControl {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-    uint256 private immutable _cap;
 
     /**
      * @dev Sets the value of the `cap`. This value is immutable, it can only be
      * set once during construction.
      */
-    constructor(uint256 cap_) ERC20("CreditCapital Platform Token", "CAPL") {
-        require(cap_ > 0, "ERC20Capped: cap is 0");
-        _cap = cap_;
-
+    constructor(uint256 cap_) ERC20("CreditCapital Platform Token", "CAPL") ERC20Capped(cap_) {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(MINTER_ROLE, msg.sender);
-    }
-
-    /**
-     * @dev Returns the cap on the token's total supply.
-     */
-    function cap() public view virtual returns (uint256) {
-        return _cap;
     }
 
     function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
         _mint(to, amount);
     }
 
-    // Capped mint, how to merge with default above?
     /**
      * @dev See {ERC20-_mint}.
      */
-    function _mint(address account, uint256 amount) internal virtual override {
-        require(ERC20.totalSupply() + amount <= cap(), "ERC20Capped: cap exceeded");
+    function _mint(address account, uint256 amount) internal virtual override(ERC20, ERC20Capped) {
         super._mint(account, amount);
     }
 }
