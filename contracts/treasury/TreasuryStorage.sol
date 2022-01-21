7e3a595ca6cca08a3fe7f734c2801211220d4436
legendary <legendary.phoenix.icarus@gmail.com>
2022-01-21 14:11:04 +0300

    update: TreasuryStorage loan function

--- contracts/treasury/TreasuryStorage.sol
+++ contracts/treasury/TreasuryStorage.sol
@@ -9,6 +9,7 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract TreasuryStorage is AccessControl {
     using SafeERC20 for IERC20;
+    bytes32 public constant FUND = keccak256("FUND");
 
     struct UserPosition {
         uint256 totalAmount;
@@ -16,7 +17,8 @@ contract TreasuryStorage is AccessControl {
         uint256 loanedAmount; // amount that has been taken out of the treasury storage as a loan
     }
 
-    mapping(address => UserPosition) UserPositions;
+    // Mapping from user to userpostion of the token
+    mapping(address => mapping(address => UserPosition)) UserPositions;
 
     // treasury shares represent a users percentage amount in the treasury pot
     IERC20 treasuryShares;
@@ -32,14 +34,41 @@ contract TreasuryStorage is AccessControl {
      */
     function deposit(uint256 _amount) external {}
 
-    function addUserPosition(address _token, uint256 _totalAmount) external {}
+    function addUserPosition(address _token, address _user, uint256 _totalAmount, uint256 _rewardDebt) external {
+        require(!this.checkIfUserPositionExists(_token), "The user position is already exists");
+
+        UserPositions[_user][_token] = UserPosition({
+            totalAmount: _totalAmount,
+            rewardDebt: _rewardDebt,
+            loanedAmount: 0
+        });
+    }
 
     /**
         @dev - this function transfers _amount to the user and updates the user position to denote the loaned amount and change in contract balance.
      */
-    function loan(address _token, uint256 _amount) external {}
+    function loan(
+        address _token,
+        address _user,
+        uint256 _amount
+    ) external onlyRole(FUND) {
+        require(IERC20(_token).balanceOf(address(this)) > _amount, "The amount exceed the treasury balance.");
+
+        UserPosition storage userPosition = UserPositions[_user][_token];
+        userPosition.loanedAmount += _amount;
+        
+        IERC20(_token).safeTransferFrom(address(this), _user, _amount);
+    }
 
     function getTokenSupply(address _token) external view returns (uint256) {
         return IERC20(_token).balanceOf(address(this));
+    }
+
+    function checkIfUserPositionExists(address _token)
+        external
+        view
+        returns (bool)
+    {
+        return UserPositions[msg.sender][_token].totalAmount > 0;
     }
 }
