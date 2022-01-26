90e2955839f60203240efb12a80584d3c1c9507b
legendary <legendary.phoenix.icarus@gmail.com>
2022-01-26 08:53:57 +0300

    test: CCPT token

--- test/tokens/creditCapitalPlatformToken.js
+++ test/tokens/creditCapitalPlatformToken.js
@@ -3,56 +3,74 @@ const { toUtf8Bytes } = require("ethers/lib/utils");
 const { ethers } = require("hardhat");
 
 const deployContract = async (contract, params) => {
-  let c = await ethers.getContractFactory(contract);
-  if (params) c = await c.deploy(params)
-  else c = await c.deploy()
-  return await c.deployed()
+	let c = await ethers.getContractFactory(contract);
+	if (params) c = await c.deploy(...params);
+	else c = await c.deploy();
+	return await c.deployed();
+}
+  
+const deployContracts = async (cap) => {
+	const capl = await deployContract("CreditCapitalPlatformToken", [cap])
+	return capl;
 }
 
-describe("Credit Capital Platform Token", async function () {
+describe("Credit Capital Platform Token", function () {
+	let deployer;
+	let minter;
+	let nonMinter;
+	let capl;
+	let cap;
+	let minterRole;
+
 	beforeEach(async function () {
-		// set supply limit
-		const cap = 10000;
+		cap = 10000;
 
+		[deployer, minter, nonMinter] = await ethers.getSigners();
 		// deploy token contract
-		const creditCapitalPlatformToken = await deployContract("CreditCapitalPlatformToken", cap);
+		capl = await deployContracts(cap);
 
-		// get test accounts
-		[deployer, minter, normalUser] = await ethers.getSigners();
-
+		minterRole = ethers.utils.keccak256(ethers.utils.toUtf8Bytes('MINTER_ROLE'));
 		// grant minter role to the account
-		await creditCapitalPlatformToken.grantRole(ethers.utils.keccak256(ethers.utils.toUtf8Bytes('MINTER_ROLE')), minter.address);
+		await capl.grantRole(minterRole, minter.address);
 	});
 
 	it("Should be mintable", async function () {
 		const mintAmount = 100;
 
 		// minter mint the amount
-		await creditCapitalPlatformToken.connect(minter.address).mint(mintAmount);
+		await capl.mint(minter.address, mintAmount);
 
 		// check the minted amount
-		const minterBalance = await creditCapitalPlatformToken.balanceOf(minter.address);
-		const totalSupply = await creditCapitalPlatformToken.totalSupply();
-
-		expect(totalSupply).to.eql(mintAmount);
-		expect(minterBalance).to.eql(mintAmount);
+		const minterBalance = await capl.balanceOf(minter.address);
+		const totalSupply = await capl.totalSupply();
+		expect(totalSupply).to.equal(mintAmount);
+		expect(minterBalance).to.equal(mintAmount);
 
 		// Access Control
 		// deployer(owner) should have minter role by default
-		await creditCapitalPlatformToken.mint(mintAmount);
+		await capl.mint(deployer.address, mintAmount);
 
-		const ownerBalance = await creditCapitalPlatformToken.balanceOf(deployer.address);
-		expect(ownerBalance).to.eql(mintAmount);
+		const ownerBalance = await capl.balanceOf(deployer.address);
+		expect(ownerBalance).to.equal(mintAmount);
 
 		// mint should be failed without minter role
-		expect(await creditCapitalPlatformToken.connect(normalUser.address).mint(mintAmount)).fail();
+		try {
+			await capl.connect(nonMinter).mint(nonMinter.address, mintAmount);
+		} catch (error) {
+			const revert = new RegExp("AccessControl: account " + nonMinter.address.toLowerCase() + " is missing role " + minterRole);
+			expect(error.message).match(revert);
+		}
 	});
 
 	it("Should be capped", async function () {
 		const mintAmount = 100;
 
 		// mint should be impossible
-		expect(await creditCapitalPlatformToken.mint(cap + mintAmount)).fail();
+		try {
+			await capl.mint(deployer.address, cap + mintAmount);
+		} catch (error) {
+			expect(error.message).match(/ERC20Capped: cap exceeded/);
+		}
 	});
 
 	it("Should be burnable", async function () {
@@ -60,21 +78,20 @@ describe("Credit Capital Platform Token", async function () {
 		const mintAmount = 10000;
 
 		// owner mint the amount
-		await creditCapitalPlatformToken.mint(mintAmount);
-
+		await capl.mint(deployer.address, mintAmount);
 		// burn amount from the deployer balance
-		await creditCapitalPlatformToken.burn(burnAmount);
+		await capl.burn(burnAmount);
 
 		// check the balance
-		expect(await creditCapitalPlatformToken.balanceOf(deployer.address)).to.eql(mintAmount - burnAmount);
-		expect(await creditCapitalPlatformToken.totalSupply()).to.eql(mintAmount - burnAmount);
+		expect(await capl.balanceOf(deployer.address)).to.equal(mintAmount - burnAmount);
+		expect(await capl.totalSupply()).to.equal(mintAmount - burnAmount);
 
 		// burnFrom
-		await creditCapitalPlatformToken.approve(normalUser, burnAmount);
-		await creditCapitalPlatformToken.burnFrom(deployer.address, burnAmount);
+		await capl.approve(nonMinter.address, burnAmount);
+		await capl.connect(nonMinter).burnFrom(deployer.address, burnAmount);
 		
 		// the normal user can burn from the deployer balance
-		expect(await creditCapitalPlatformToken.balanceOf(deployer.address)).to.eql(mintAmount - (burnAmount * 2));
-		expect(await creditCapitalPlatformToken.totalSupply()).to.eql(mintAmount - (burnAmount * 2));
+		expect(await capl.balanceOf(deployer.address)).to.equal(mintAmount - (burnAmount * 2));
+		expect(await capl.totalSupply()).to.equal(mintAmount - (burnAmount * 2));
 	});
 })
