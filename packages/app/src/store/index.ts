7667ff6faf493865f7969b06c51f937092ac4ce4
enriquetalent <enriquevupwork@gmail.com>
2022-02-03 03:31:14 +0300

    remove vault.ts

--- packages/app/src/store/index.ts
+++ packages/app/src/store/index.ts
@@ -2,7 +2,6 @@ import { InjectionKey } from "vue";
 import { createStore, useStore as baseUseStore, Store } from "vuex";
 import accounts from "./modules/accounts";
 import contracts from "./modules/contracts";
-import vault from "./modules/vault";
 
 // define injection key
 export const key: InjectionKey<Store<any>> = Symbol();
@@ -11,7 +10,6 @@ export const store = createStore<any>({
   modules: {
     accounts: accounts,
     contracts: contracts,
-    vault: vault,
   },
   state: {},
 });
