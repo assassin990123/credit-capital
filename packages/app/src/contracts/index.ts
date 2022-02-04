fa23c135ab8883fe52dd495ded29ee149d7149be
Casey Sullivan <casey.su87@gmail.com>
2022-02-04 14:52:26 -0500

    move constant addresses

--- packages/app/src/contracts/index.ts
+++ packages/app/src/contracts/index.ts
@@ -23,4 +23,40 @@ const caplUSDCPoolId: { [key: number]: string } = {
   42: "0x270c10cb22cf7dfcbb6435b9a0886bd05e5818e9000200000000000000000624",
 };
 
-export { capl, vault, rewards, balancerVault, caplUSDCPoolId };
+const poolWETH_USDC: { [key: number]: string } = {
+  1: "",
+  42: "0x3a19030ed746bd1c3f2b0f996ff9479af04c5f0a000200000000000000000004",
+};
+
+const poolBAL_WETH: { [key: number]: string } = {
+  1: "",
+  42: "0x61d5dc44849c9c87b0856a2a311536205c96c7fd000200000000000000000000",
+};
+
+const swapTokenBAL: { [key: number]: string } = {
+  1: "",
+  42: "0x41286Bb1D3E870f3F750eB7E1C25d7E48c8A1Ac7",
+};
+
+const swapTokenUSDC: { [key: number]: string } = {
+  1: "",
+  42: "0xc2569dd7d0fd715B054fBf16E75B001E5c0C1115",
+};
+
+const swapTokenWETH: { [key: number]: string } = {
+  1: "",
+  42: "0xdFCeA9088c8A88A76FF74892C1457C17dfeef9C1",
+};
+
+export {
+  capl,
+  vault,
+  rewards,
+  balancerVault,
+  caplUSDCPoolId,
+  poolWETH_USDC,
+  poolBAL_WETH,
+  swapTokenBAL,
+  swapTokenUSDC,
+  swapTokenWETH,
+};
