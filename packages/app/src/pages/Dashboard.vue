bad88145ad1f272dd0d657fd7ab706aa07b62d72
HeniKDeveloper <kothariheni3@gmail.com>
2022-02-07 18:35:52 +0530

    Dashboard design complated in Dashboard.vue

--- packages/app/src/pages/Dashboard.vue
+++ packages/app/src/pages/Dashboard.vue
@@ -1,16 +1,16 @@
 <template>
-  <div class="dashboard-container inner-container">
+  <div class="dashboard-container inner-container dashboard-cus-main">
     <div class="dashboard-daily-earning">
       <div class="dashboard-daily-earning-panel">
-        <div>
+        <div class="title-cus">
           <h2>DAILY EARNINGS</h2>
           <div class="dashboard-daily-earning-panel-value">0</div>
         </div>
-        <div>
+        <div class="title-cus">
           <h2>APR</h2>
           <div class="dashboard-daily-earning-panel-value">0.0000%</div>
         </div>
-        <div>
+        <div class="title-cus">
           <h2>TVL</h2>
           <div class="dashboard-daily-earning-panel-value">0.0000 USD</div>
         </div>
@@ -22,19 +22,33 @@
         </div>
         <div class="dashboard-daily-earning-capl-content">
           <div class="dashboard-daily-earning-capl-content-row">
-            <div class="dashboard-daily-earning-capl-content-title">Your Balance</div>
-            <div class="dashboard-daily-earning-capl-content-value">0.0000 CAPL (0.0000 USD)</div>
+            <div class="dashboard-daily-earning-capl-content-title">
+              Your Balance
+            </div>
+            <div class="dashboard-daily-earning-capl-content-value">
+              0.0000 CAPL (0.0000 USD)
+            </div>
           </div>
           <div class="dashboard-daily-earning-capl-content-row">
-            <div class="dashboard-daily-earning-capl-content-title">Your Staked Balance</div>
-            <div class="dashboard-daily-earning-capl-content-value">0.0000 USDC-CAPL Shares (0.0000 USD)</div>
+            <div class="dashboard-daily-earning-capl-content-title">
+              Your Staked Balance
+            </div>
+            <div class="dashboard-daily-earning-capl-content-value">
+              0.0000 USDC-CAPL Shares (0.0000 USD)
+            </div>
           </div>
           <div class="dashboard-daily-earning-capl-content-row">
-            <div class="dashboard-daily-earning-capl-content-title">Daily Revenue</div>
-            <div class="dashboard-daily-earning-capl-content-value">0 CAPL (0.0000 USD)</div>
+            <div class="dashboard-daily-earning-capl-content-title">
+              Daily Revenue
+            </div>
+            <div class="dashboard-daily-earning-capl-content-value">
+              0 CAPL (0.0000 USD)
+            </div>
           </div>
           <div class="dashboard-daily-earning-capl-content-row">
-            <div class="dashboard-daily-earning-capl-content-title">Daily Yield</div>
+            <div class="dashboard-daily-earning-capl-content-title">
+              Daily Yield
+            </div>
             <div class="dashboard-daily-earning-capl-content-value">∞%</div>
           </div>
         </div>
@@ -45,7 +59,9 @@
         <h2>VAULT</h2>
         <div>
           <div>Your Stakes USDC-CAPL Shares</div>
-          <div class="dashboard-revenue-projection-value">0.0000 LP Shares (0.0000) USD</div>
+          <div class="dashboard-revenue-projection-value">
+            0.0000 LP Shares (0.0000) USD
+          </div>
         </div>
         <div>
           <div>Your Daily Revenue</div>
@@ -74,12 +90,16 @@
         </div>
       </div>
     </div>
-    <h2 class="text-center">PORTFOLIO</h2>
+    <div class="protfolio-title-main">
+      <h2 class="text-center">PORTFOLIO</h2>
+    </div>
     <div class="dashboard-portfolio">
       <div class="dashboard-portfolio-section">
         <div class="dashboard-portfolio-section-address">
           <h2>WALLET ADDRESS</h2>
-          <div class="dashboard-portfolio-section-address-value">0x00000000000000000000</div>
+          <div class="dashboard-portfolio-section-address-value">
+            0x00000000000000000000
+          </div>
         </div>
         <div class="dashboard-portfolio-section-title">Wallet Assets</div>
         <div class="dashboard-portfolio-section-panel">
@@ -107,7 +127,7 @@
             <div>0.0000 CAPL (0.0000 USD)</div>
           </div>
         </div>
-        <button>Claim Rewards</button>
+        <button type="submit" class="reward-btn">Claim Rewards</button>
       </div>
       <div class="dashboard-portfolio-section">
         <div class="dashboard-portfolio-capl-row">
@@ -141,7 +161,9 @@
         </div>
       </div>
     </div>
-    <h2 class="text-center">PLATFORM</h2>
+    <div class="protfolio-title-main">
+      <h2 class="text-center">PLATFORM</h2>
+    </div>
     <div class="dashboard-platform">
       <div class="dashboard-platform-section">
         <div class="dashboard-platform-header">
@@ -150,7 +172,7 @@
             <div>$1.0357 USD</div>
             <div>Current Price</div>
           </div>
-          <button>Buy</button>
+          <button class="buy-btn">Buy</button>
         </div>
         <div class="dashboard-portfolio-section-title">Tokenomics</div>
         <div class="dashboard-platform-token">
@@ -215,33 +237,53 @@
 <script lang="ts" setup></script>
 
 <style>
-.dashboard-container {
-  position: absolute;
-  top: 0;
-  left: 0;
-  display: flex;
-  flex-direction: column;
-  width: 70%;
-  margin-left: 15%;
-  margin-right: 15%;
+.dashboard-container.dashboard-cus-main {
+  padding: 100px;
+  background-color: #fdf6e4;
 }
 
-.dashboard-daily-earning {
+.dashboard-cus-main .dashboard-daily-earning {
   display: flex;
   flex-direction: row;
   width: 100%;
-  height: 35vh;
+  padding: 40px 30px;
+  background-color: #fff;
+  -webkit-box-shadow: 0px 0px 22px 0px rgba(0, 0, 0, 0.17);
+  -moz-box-shadow: 0px 0px 22px 0px rgba(0, 0, 0, 0.17);
+  box-shadow: 0px 0px 22px 0px rgba(0, 0, 0, 0.17);
+  border-radius: 20px;
 }
 
-.dashboard-daily-earning-panel {
+.dashboard-cus-main .dashboard-daily-earning-panel {
   display: flex;
   flex-direction: column;
   justify-content: space-around;
-  border: 1px solid #000000;
   margin-right: 20px;
+  padding: 40px 20px;
   width: 35%;
   text-align: center;
-  font-size: 14px;
+  background-color: #fdf6e4;
+  border-radius: 20px;
+  -webkit-box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.1);
+  -moz-box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.1);
+  box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.1);
+}
+.dashboard-cus-main .title-cus {
+  margin: 0 0 25px 0;
+  padding: 0 0 25px 0;
+  border-bottom: 1px solid #e6e6e6;
+}
+.dashboard-cus-main .title-cus h2 {
+  color: #000000;
+  margin: 0 0 10px 0;
+}
+.dashboard-cus-main .dashboard-daily-earning-panel-value {
+  font-size: 22px;
+  font-weight: bold;
+  color: #565972;
+}
+.dashboard-cus-main .title-cus:last-child {
+  border: none;
 }
 
 .dashboard-daily-earning-capl {
@@ -250,51 +292,75 @@
   flex: 1;
 }
 
-.dashboard-daily-earning-capl-header {
-  border: 1px solid #000000;
-  margin-bottom: 15px;
+.dashboard-cus-main .dashboard-daily-earning-capl-header {
+  margin-bottom: 35px;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
-  padding: 10px;
+  padding: 15px 30px;
+  background-color: #fff;
+  border-radius: 15px;
+  -webkit-box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.1);
+  -moz-box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.1);
+  box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.1);
 }
-
-.dashboard-daily-earning-capl-content {
+.dashboard-cus-main .dashboard-daily-earning-capl-header h2 {
+  margin: 0px;
+  color: #000000;
+}
+.dashboard-cus-main .dashboard-daily-earning-capl-content {
   flex: 1;
-  border: 1px solid #000000;
-  padding: 10px;
+  padding: 15px 30px;
+  background-color: #fff;
+  border-radius: 15px;
+  -webkit-box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.1);
+  -moz-box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.1);
+  box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.1);
   display: flex;
   flex-direction: column;
 }
 
-.dashboard-daily-earning-capl-content-row {
+.dashboard-cus-main .dashboard-daily-earning-capl-content-row {
   display: flex;
   flex-direction: row;
   justify-content: space-between;
   width: 100%;
-  border-bottom: 1px solid #000000;
-  margin-top: 10px;
-  padding-bottom: 8px;
+  border-bottom: 1px solid #e6e6e6;
+  margin-bottom: 20px;
+  padding: 15px;
+  color: #000000;
 }
 
-.dashboard-revenue-projection {
+.dashboard-cus-main .dashboard-revenue-projection {
   display: flex;
   flex-direction: row;
-  border: 1px solid #000000;
-  padding: 10px 20px;
-  margin-top: 40px;
-  height: 30vh;
+  padding: 40px 30px;
+  background-color: #fff;
+  box-shadow: 0px 0px 22px 0px rgb(0 0 0 / 17%);
+  border-radius: 20px;
+  margin-top: 60px;
 }
 
-.dashboard-revenue-projection-vault {
+.dashboard-cus-main .dashboard-revenue-projection-vault {
   display: flex;
   flex-direction: column;
   justify-content: space-around;
-  border: 1px solid #000000;
   width: 25%;
   margin-right: 40px;
   text-align: center;
-  padding: 10px 15px;
+  padding: 40px 20px;
+  background-color: #fdf6e4;
+  border-radius: 20px;
+  box-shadow: 0px 0px 10px 0px rgb(0 0 0 / 10%);
+}
+.dashboard-cus-main .dashboard-revenue-projection-vault h2 {
+  color: #000;
+  margin: 0 0 15px 0;
+}
+.dashboard-cus-main .dashboard-revenue-projection-vault div {
+  color: #565972;
+  font-size: 18px;
+  font-weight: 600;
 }
 
 .dashboard-revenue-projection-content {
@@ -304,33 +370,103 @@
   justify-content: space-around;
   text-align: center;
 }
+.dashboard-cus-main .dashboard-revenue-projection-content h2 {
+  color: #000000;
+}
 
-.dashboard-revenue-projection-content-row {
+.dashboard-cus-main .dashboard-revenue-projection-content-row {
   display: flex;
   flex-direction: row;
-  justify-content: space-between;
+  justify-content: center;
 }
 
-.dashboard-revenue-projection-content-column {
-  border: 1px solid #000000;
-  padding: 10px 0;
+.dashboard-cus-main .dashboard-revenue-projection-content-column {
+  padding: 40px;
+  background-color: #fff;
+  border-radius: 15px;
+  border: 2px solid #ff8900;
+  margin: 0 25px;
 }
-
-.dashboard-revenue-projection-value {
+.dashboard-cus-main .dashboard-revenue-projection-content-column > div {
+  color: #000;
+  font-weight: bold;
+}
+.dashboard-cus-main .protfolio-title-main {
+  margin: 50px 0;
+}
+.dashboard-cus-main .protfolio-title-main h2 {
+  color: #000000;
+  margin: 0px;
+}
+.dashboard-cus-main .dashboard-portfolio-section {
+  padding: 40px 30px;
+  background-color: #fff;
+  box-shadow: 0px 0px 22px 0px rgb(0 0 0 / 17%);
+  border-radius: 20px;
+  border: none;
+}
+.dashboard-cus-main .dashboard-portfolio-section-address h2 {
+  color: #000000;
+  margin: 0px;
+}
+.dashboard-cus-main .dashboard-revenue-projection-value {
   font-size: 20px;
   font-weight: 700;
 }
+.dashboard-cus-main .reward-btn {
+  display: inline-block;
+  padding: 15px;
+  border-radius: 50px;
+  border: none;
+  background: rgba(255, 154, 2, 1);
+  background: -moz-linear-gradient(
+    left,
+    rgba(255, 154, 2, 1) 0%,
+    rgba(255, 197, 6, 1) 100%
+  );
+  background: --webkit-gradient(
+    left top,
+    right top,
+    color-stop(0%, rgba(255, 154, 2, 1)),
+    color-stop(100%, rgba(255, 197, 6, 1))
+  );
+  background: -webkit-linear-gradient(
+    left,
+    rgba(255, 154, 2, 1) 0%,
+    rgba(255, 197, 6, 1) 100%
+  );
+  background: -o-linear-gradient(
+    left,
+    rgba(255, 154, 2, 1) 0%,
+    rgba(255, 197, 6, 1) 100%
+  );
+  background: -ms-linear-gradient(
+    left,
+    rgba(255, 154, 2, 1) 0%,
+    rgba(255, 197, 6, 1) 100%
+  );
+  background: linear-gradient(
+    to right,
+    rgba(255, 154, 2, 1) 0%,
+    rgba(255, 197, 6, 1) 100%
+  );
+  filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#ff9a02', endColorstr='#ffc506', GradientType=1);
+  text-align: center;
+  font-weight: bold;
+  font-size: 18px;
+  color: #fff;
+  min-width: 250px;
+  margin: 40px auto;
+}
 
-.dashboard-portfolio {
+.dashboard-cus-main .dashboard-portfolio {
   display: flex;
   flex-direction: row;
   justify-content: space-between;
   margin-top: 30px;
 }
-
 .dashboard-portfolio-section {
   width: 45%;
-  border: 1px solid #000000;
   display: flex;
   flex-direction: column;
   padding: 20px 15px;
@@ -340,20 +476,39 @@
   text-align: center;
 }
 
-.dashboard-portfolio-section-address {
+.dashboard-cus-main .dashboard-portfolio-section-address {
   display: flex;
   flex-direction: row;
   justify-content: space-between;
 }
+.dashboard-cus-main .dashboard-portfolio-section-title {
+  font-weight: 700;
+  color: #ff8900;
+}
+.dashboard-cus-main .dashboard-portfolio-section-panel {
+  padding: 40px 30px;
+  background-color: #fff;
+  box-shadow: 0px 0px 20px 0px rgb(0 0 0 / 9%);
+  border-radius: 20px;
+  border: none;
+}
+.dashboard-cus-main .dashboard-portfolio-section-panel-row {
+  padding: 15px;
+  border-bottom: 1px solid #e6e6e6;
+  margin: 0 0 20px 0;
+}
 
-.dashboard-portfolio-section-address h2 {
+.dashboard-cus-main .dashboard-portfolio-section-address h2 {
   margin: 10px 0;
 }
 
-.dashboard-portfolio-section-address-value {
-  border: 1px solid #000000;
-  padding: 0 10px;
+.dashboard-cus-main .dashboard-portfolio-section-address-value {
+  padding: 15px;
   line-height: 44px;
+  color: #000;
+  font-weight: bold;
+  border: 2px solid #ff8900;
+  border-radius: 15px;
 }
 
 .dashboard-portfolio-section-title {
@@ -375,31 +530,47 @@
 }
 
 .dashboard-portfolio-section button {
-	margin-top: 20px;
-}
-
-.dashboard-portfolio-capl-row {
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-}
-
-.dashboard-portfolio-capl-panel {
-  border: 1px solid #000000;
-  padding: 10px 10px 30px 10px;
   margin-top: 20px;
 }
 
-.dashboard-portfolio-capl-panel-row {
+.dashboard-cus-main .dashboard-portfolio-capl-row {
   display: flex;
   flex-direction: row;
   justify-content: space-between;
-  padding-bottom: 10px;
-  border-bottom: 1px solid #000000;
-  margin-top: 10px;
+  padding: 30px;
+  background: #fff;
+  box-shadow: 0px 0px 20px 0px rgb(0 0 0 / 9%);
+  border-radius: 20px;
+}
+.dashboard-cus-main .dashboard-portfolio-capl-row div {
+  font-size: 18px;
+  font-weight: bold;
+  color: #000;
 }
 
-.dashboard-platform {
+.dashboard-cus-main .dashboard-portfolio-capl-panel {
+  padding: 40px 30px;
+  background-color: #fff;
+  box-shadow: 0px 0px 20px 0px rgb(0 0 0 / 9%);
+  border-radius: 20px;
+  border: none;
+  margin-top: 40px;
+}
+
+.dashboard-cus-main .dashboard-portfolio-capl-panel-row {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 15px;
+  border-bottom: 1px solid #e6e6e6;
+  margin: 0 0 15px 0;
+}
+.dashboard-cus-main .dashboard-portfolio-capl-panel-row div {
+  font-weight: bold;
+  color: #000;
+}
+
+.dashboard-cus-main .dashboard-platform {
   display: flex;
   flex-direction: row;
   justify-content: space-between;
@@ -407,12 +578,15 @@
   margin-bottom: 50px;
 }
 
-.dashboard-platform-section {
+.dashboard-cus-main .dashboard-platform-section {
   width: 45%;
-  border: 1px solid #000000;
+  padding: 40px 30px;
+  background-color: #fff;
+  box-shadow: 0px 0px 22px 0px rgb(0 0 0 / 17%);
+  border-radius: 20px;
+  border: none;
   display: flex;
   flex-direction: column;
-  padding: 20px 15px;
 }
 
 .dashboard-platform-header {
@@ -420,13 +594,51 @@
   flex-direction: row;
   justify-content: space-between;
 }
-
+.dashboard-cus-main .dashboard-platform-header h2 {
+  color: #000;
+}
+.dashboard-cus-main .dashboard-platform-header div {
+  color: #000;
+  font-size: 19px;
+  font-weight: bold;
+}
+.dashboard-cus-main .buy-btn {
+  display: block;
+  padding: 10px 80px;
+  border-radius: 50px;
+  border: none;
+  background: rgba(255, 154, 2, 1);
+  background: --webkit-gradient(
+    left top,
+    right top,
+    color-stop(0%, rgba(255, 154, 2, 1)),
+    color-stop(100%, rgba(255, 197, 6, 1))
+  );
+  background: linear-gradient(
+    to right,
+    rgba(255, 154, 2, 1) 0%,
+    rgba(255, 197, 6, 1) 100%
+  );
+  filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#ff9a02', endColorstr='#ffc506', GradientType=1);
+  text-align: center;
+  font-weight: bold;
+  font-size: 22px;
+  color: #fff;
+  line-height: normal;
+  height: 65px;
+  min-height: auto;
+  cursor: pointer;
+}
 .dashboard-platform-header h2 {
   margin: 0;
 }
 
-.dashboard-platform-token {
-  border: 1px solid #000000;
+.dashboard-cus-main .dashboard-platform-token {
+  padding: 40px 30px;
+  background-color: #fff;
+  box-shadow: 0px 0px 20px 0px rgb(0 0 0 / 9%);
+  border-radius: 20px;
+  border: none;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
@@ -436,22 +648,18 @@
   width: 50%;
   padding: 10px;
 }
-
-.dashboard-platform-token-column h2 {
-  margin: 0;
+.dashboard-cus-main .dashboard-platform-token-column h3 {
+  color: #000;
+  font-weight: bold;
 }
-
-.dashboard-platform-token-market {
-}
-
-.dashboard-platform-token-market-row {
-  margin-bottom: 5px;
-  padding-bottom: 10px;
+.dashboard-cus-main .dashboard-platform-token-market-row {
+  padding: 15px;
+  border-bottom: 1px solid #e6e6e6;
+  margin: 0px;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
-  border-bottom: 1px solid #000000;
-  font-size: 14px;
+  font-size: 16px;
 }
 
 .dashboard-platform-assets {
@@ -460,10 +668,33 @@
   justify-content: space-between;
   margin-bottom: 10px;
 }
+.dashboard-cus-main .dashboard-platform-assets {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 30px;
+  background: #fff;
+  box-shadow: 0px 0px 20px 0px rgb(0 0 0 / 9%);
+  border-radius: 20px;
+}
+.dashboard-cus-main .dashboard-platform-assets div {
+  font-size: 18px;
+  font-weight: bold;
+  color: #000;
+}
 
-.dashboard-platform-assets-panel {
+.dashboard-cus-main .dashboard-platform-assets-panel {
   padding: 10px;
-  border: 1px solid #000000;
+  padding: 30px;
+  background: #fff;
+  box-shadow: 0px 0px 20px 0px rgb(0 0 0 / 9%);
+  border-radius: 20px;
+  margin: 30px 0;
+}
+.dashboard-cus-main .dashboard-platform-assets-panel div {
+  font-size: 18px;
+  font-weight: bold;
+  color: #000;
 }
 
 .dashboard-platform-assets-panel-row {
@@ -472,12 +703,17 @@
   justify-content: space-between;
 }
 
-.dashboard-platform-assets-panel-row-border {
+.dashboard-cus-main .dashboard-platform-assets-panel-row-border {
   display: flex;
   flex-direction: row;
   justify-content: space-between;
-  border-bottom: 1px solid #000000;
-  margin-bottom: 10px;
-  padding-bottom: 5px;
+  border-bottom: 1px solid #e6e6e6;
+  margin: 0 0 15px 0;
+  padding: 15px;
+}
+.dashboard-cus-main .dashboard-platform-assets-panel-row-border div {
+  font-weight: bold;
+  color: #000;
+  font-size: 16px;
 }
 </style>
