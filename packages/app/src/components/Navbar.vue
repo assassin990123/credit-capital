5a10e52e613704b8150c22cb1698da22214b6bdc
Graham Jenkins <graham@mrgraham.net>
2022-02-08 13:36:03 -0800

    Home link updates

--- packages/app/src/components/Navbar.vue
+++ packages/app/src/components/Navbar.vue
@@ -5,9 +5,9 @@
       <div class="container">
         <div class="acavo-responsive-menu">
           <div class="logo">
-            <a href="/index.html">
+            <router-link to="/">
               <img src="/images/logo-white.png" alt="logo" />
-            </a>
+            </router-link>
           </div>
         </div>
       </div>
@@ -16,13 +16,12 @@
     <div class="acavo-nav">
       <div class="container">
         <nav class="navbar navbar-expand-md navbar-light">
-          <a class="navbar-brand" href="/index.html">
+          <router-link to="/">
             <img src="/images/logo-white.png" alt="logo" />
-          </a>
+          </router-link>
 
           <div class="collapse navbar-collapse mean-menu">
             <ul class="navbar-nav">
-              <li class="nav-item"><router-link to="/">Home</router-link></li>
               <li class="nav-item">
                 <router-link to="dashboard">Dashboard</router-link>
               </li>
