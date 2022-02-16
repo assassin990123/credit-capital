61c5bcfeca95281e6b11dbd1905f37c529b35063
Elo <elo@Elos-MacBook-Pro.local>
2022-02-15 20:40:35 -0800

    tests fix

--- packages/sol/test/standard/rewards.js
+++ packages/sol/test/standard/rewards.js
@@ -92,11 +92,6 @@ describe("Rewards Vault", function () {
     // Access Control
     // set rewards as MINTER_ROLE role in capl
     await capl.grantRole(capl.MINTER_ROLE(), rewards.address.toLowerCase());
-    // set user as MINTER_ROLE role in rewards.
-    await rewards.grantRole(
-      await rewards.MINTER_ROLE(),
-      user.address.toLowerCase()
-    );
 
     // fast forward
     await network.provider.send("evm_increaseTime", [3600]);
