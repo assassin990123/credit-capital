b1cb101807ade3df9bdf486d40f02888ae4b0e28
Graham Jenkins <graham@mrgraham.net>
2022-03-08 19:19:48 -0800

    Updated format function

--- packages/app/src/utils/index.ts
+++ packages/app/src/utils/index.ts
@@ -1,18 +1,15 @@
 export const format = (n: any) => {
-  if (n < 1e3) {
-    return Number(n).toFixed(3);
-  }
-  if (n >= 1e3 && n < 1e6) {
-    return +(n / 1e3).toFixed(1) + " K";
+  if (n < 1e6) {
+    return Number(n).toFixed(2);
   }
   if (n >= 1e6 && n < 1e9) {
-    return +(n / 1e6).toFixed(1) + " M";
+    return +(n / 1e6).toFixed(2) + " M";
   }
   if (n >= 1e9 && n < 1e12) {
-    return +(n / 1e9).toFixed(1) + " B";
+    return +(n / 1e9).toFixed(2) + " B";
   }
   if (n >= 1e12) {
-    return +(n / 1e12).toFixed(1) + " T";
+    return +(n / 1e12).toFixed(2) + " T";
   }
 };
 
